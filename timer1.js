62f2f153a0bcfa0979bd7426d99201dd66131c4f
Martha Staus <mjstaus@gmail.com>
2021-11-18 17:39:14 -0800

    refactored if statement to filter pattern

--- timer1.js
+++ timer1.js
@@ -1,10 +1,12 @@
-const args = process.argv;
-let relevantArgs = args.slice(2);
+const args = process.argv.slice(2);
 
-for (let arg of relevantArgs) {
-  if (Number(arg) && Number(arg) > 0) {
-    setTimeout(() => {
-      process.stdout.write("\x07");
-    }, arg * 1000);
+for (let arg of args) {
+  if (!Number(arg) || Number(arg) < 0) {
+    continue;
   }
+
+  setTimeout(() => {
+    process.stdout.write("\x07");
+    console.log("beep", arg);
+  }, arg * 1000);
 }
